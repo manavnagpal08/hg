bb4471e1db8a6bbb701d00e6f9b1a7be0eab4b2a
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-07-17 13:38:24 +0530

    Update screener.py

--- screener.py
+++ screener.py
@@ -1103,8 +1103,8 @@ def save_certificate_data_to_firestore(certificate_data):
 
 def send_certificate_email(recipient_email, candidate_name, certificate_id, score, certificate_html_content, attach_html_file=False):
     # Retrieve Gmail credentials from Streamlit secrets
-    gmail_address = st.secrets["GMAIL_ADDRESS"]
-    gmail_app_password = st.secrets["GMAIL_APP_PASSWORD"]
+    gmail_address = "screenerpro.ai@gmail.com"
+    gmail_app_password = "udwi life nbdv kgdt"
 
     if not gmail_address or not gmail_app_password:
         st.error("Email sending is not configured. Please ensure your Gmail address and App Password secrets are set in Streamlit.")
